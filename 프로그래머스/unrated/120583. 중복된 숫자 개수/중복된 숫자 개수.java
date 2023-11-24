fe142e1ff41519e9919dda7e77e0e280ac3452c6
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2023-11-24 17:40:40 +0900

    [level unrated] Title: 중복된 숫자 개수, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        
+        for (int value : array) {
+            if (value == n) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}

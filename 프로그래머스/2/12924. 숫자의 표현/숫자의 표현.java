e7739ee249524b36af41462cd077b06ee5e0f03e
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2023-12-13 22:51:15 +0900

    [level 2] Title: 숫자의 표현, Time: 1.38 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -3,9 +3,9 @@ class Solution {
         int answer = 0;
         int start = 1;
         
-        while(start <= n/2) {
+        while(start < n) {
             int sum = 0;
-            for (int i = start; i <= n/2+1; i++) {
+            for (int i = start; i < n; i++) {
                 sum += i;
                 if (sum == n) {
                     answer++;

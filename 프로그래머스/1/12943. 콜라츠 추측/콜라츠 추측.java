0dc82e4c07dffc6d6f3415f0e04f7d1a3f1317db
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2024-04-18 17:56:28 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int solution(long num) {
+    public int solution(int num) {
         int answer = 0;
+        long lnum = num;
         
         for (int i = 0; i <= 500; i++) {
             answer = i;
             
-            if (num == 1) {
+            if (lnum == 1) {
                 break;
             }
             
-            if (num % 2 == 0) {
-                num /= 2;
+            if (lnum % 2 == 0) {
+                lnum /= 2;
             } else {
-                num = num * 3 + 1;
+                lnum = lnum * 3 + 1;
             }            
         }
         
-        if (num != 1) {
+        if (lnum != 1) {
             answer = -1;
         }
         

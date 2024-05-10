148faa9ddb18c327d8184f381cd9f146ecaca758
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2024-05-10 23:37:13 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 134.18 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] food_times, long k) {
+        int answer = 0;
+        int[] sortFood = food_times.clone();
+        
+        Arrays.sort(sortFood);
+        
+        long roof = 0;
+        long roofCount = 0;
+        long count = 0;
+        for (int i = 0; i < sortFood.length; i++) {
+            for (int j = 0; j < sortFood[i] - roof; j++) {
+                count += (sortFood.length - i);
+                roofCount++;
+
+                if (count > k) {
+                    break;
+                }
+            }
+            
+            if (count > k) {
+                count -= (sortFood.length - i);
+                break;
+            }
+            
+            roof += (sortFood[i] - roof);
+        }
+        
+        for (int i = 0; i < food_times.length; i++) {
+            if (food_times[i] - roof > 0) {
+                count++;
+            }
+            if (count > k) {
+                answer = i + 1;
+                break;
+            }
+        }
+        
+        if (answer == 0) {
+            answer = -1;
+        }
+        
+        return answer;
+    }
+}

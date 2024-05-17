c7d309eaed573951d4810648b223e25cd008bd9a
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2024-05-17 21:22:46 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,14 +5,8 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < commands.length; i++) {
-            int[] temp = new int[commands[i][1] - commands[i][0] + 1];
-            
-            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
-                temp[j - (commands[i][0] - 1)] = array[j];
-            }
-            
-            Arrays.sort(temp);
-            
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);                
+            Arrays.sort(temp);            
             answer[i] = temp[commands[i][2] - 1];
         }
         

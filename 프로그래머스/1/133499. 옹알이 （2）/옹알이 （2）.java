5446082ea96ec741724e60df4f124575b87e1a0d
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2024-07-10 02:32:23 +0900

    [level 1] Title: 옹알이 (2), Time: 3.17 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,27 +1,11 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] baby = new String[] {"aya", "ye", "woo", "ma"};
-
-        for (int i = 0; i < babbling.length; i++) {
-            for (String s : baby) {
-                babbling[i] = babbling[i].replaceAll(s, "|");
-                if (babbling[i].contains("||")) {
-                    break;
-                } else {
-                    babbling[i] = babbling[i].replaceAll("\\|", " ");
-                }
-            }
-
-            if (!babbling[i].contains("||")) {
-                babbling[i] = babbling[i].replaceAll(" ", "");
-
-                if (babbling[i].isEmpty()) {
-                    answer++;
-                }
-            }
+        for(int i = 0; i < babbling.length; i++){
+            babbling[i] = babbling[i].replaceAll("ayaaya|yeye|woowoo|mama","1");
+            babbling[i] = babbling[i].replaceAll("aya|ye|woo|ma","");
+            if(babbling[i].isEmpty()) answer++;
         }
-
         return answer;
     }
 }

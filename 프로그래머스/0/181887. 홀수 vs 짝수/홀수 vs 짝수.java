70e5dbaedf50dd0f003532903040b8c2d9601682
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2024-08-19 21:06:30 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer1 = 0;
+        int answer2 = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) {
+                answer1 += num_list[i];
+            } else {
+                answer2 += num_list[i];
+            }
+        }
+        
+        return Math.max(answer1, answer2);
+    }
+}

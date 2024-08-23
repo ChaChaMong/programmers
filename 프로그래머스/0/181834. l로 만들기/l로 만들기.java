1a756b35f5e0e61e15f86bd2f6eff7468a20eb47
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2024-08-24 00:49:02 +0900

    [level 0] Title: l로 만들기, Time: 15.80 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,15 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder answer = new StringBuilder();
-        
-        for (int i = 0; i < myString.length(); i++) {
-            if (myString.charAt(i) < 'l') {
-                answer.append("l");
-            } else {
-                answer.append(myString.charAt(i));
-            }
-        }
-        
-        return answer.toString();
+        return myString.replaceAll("[a-k]", "l");
     }
 }
